baae8001efc7e92190839670c872e7492fc521ab
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2025-10-29 17:05:32 -0600

    Update orders.pl

--- orders.pl
+++ orders.pl
@@ -6,7 +6,7 @@
 % =========================
 next_order_code(Code) :-
     (   exists_file('orders.csv')
-    
+    ->  % Leer archivo y obtener la última línea
         csv_read_file('orders.csv', Rows, [functor(row), arity(8)]),
         (   last(Rows, LastRow),
             arg(8, LastRow, LastCode),
@@ -18,21 +18,20 @@ next_order_code(Code) :-
         )
     ;   NextNum = 1
     ),
-    format(atom(Code), 'ORD~|~`0t~d~4+', [NextNum]).
+   
+    format(atom(Code), 'ORD~`0t~d~4+', [NextNum]).
 
 % =========================
 % Registrar una orden 
 % =========================
 log_order(Date, ProductCode, ProductName, Quantity, Place, ReceiverName, Status, OrderCode) :-
     next_order_code(OrderCode),
-
     (   \+ exists_file('orders.csv')
     ->  open('orders.csv', write, Stream, [encoding(utf8)]),
         csv_write_stream(Stream, [row('Date','Code','Product','Quantity','DeliveryPlace','ReceiverName','Status','OrderCode')], []),
         close(Stream)
     ;   true
     ),
-
     open('orders.csv', append, Stream, [encoding(utf8)]),
     csv_write_stream(Stream,
         [row(Date, ProductCode, ProductName, Quantity, Place, ReceiverName, Status, OrderCode)], []),
